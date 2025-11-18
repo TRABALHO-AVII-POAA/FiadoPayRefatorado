1c485a5ee2648ca42c2bc2e7d183aada33eeb52d
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-11-18 15:46:47 -0300

    Configurando as dependências no pom.xml

---
+++ FiadoPayRefatorado/src/main/java/com/example/FiadoPayRefatorado/FiadoPayRefatoradoApplication.java
@@ -0,0 +1,13 @@
+package com.example.FiadoPayRefatorado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiadoPayRefatoradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiadoPayRefatoradoApplication.class, args);
+	}
+
+}
